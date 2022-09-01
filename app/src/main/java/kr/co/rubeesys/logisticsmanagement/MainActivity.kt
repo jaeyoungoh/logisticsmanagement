e94ad877935869d874d75deb2956267a106f30ff
오재영 <wodud.oh@gmail.com>
2022-09-02 01:33:41 +0900

    navigation menu 추가 진행중

--- app/src/main/java/kr/co/rubeesys/logisticsmanagement/MainActivity.kt
+++ app/src/main/java/kr/co/rubeesys/logisticsmanagement/MainActivity.kt
@@ -2,15 +2,55 @@ package kr.co.rubeesys.logisticsmanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import kr.co.rubeesys.logisticsmanagement.databinding.ActivityMainBinding
+import kr.co.rubeesys.logisticsmanagement.databinding.ActivityMainBinding.inflate
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private var mMainBinding:ActivityMainBinding? = null;
+    private val binding get() = mMainBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // 기본 액션바 숨김처리
-        supportActionBar?.hide()
+        binding.btnMenu.setOnClickListener{
+            binding.layoutDrawer.openDrawer(GravityCompat.END)
+        }
+        binding.naviView.setNavigationItemSelectedListener(this)
+    }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId)
+        {
+            R.id.navi_search -> Toast.makeText(applicationContext, "QR 조회", Toast.LENGTH_SHORT).show()
+            R.id.navi_packaging -> Toast.makeText(applicationContext, "포장", Toast.LENGTH_SHORT).show()
+            R.id.navi_sending -> Toast.makeText(applicationContext, "발송", Toast.LENGTH_SHORT).show()
+            R.id.navi_setting -> Toast.makeText(applicationContext, "설정", Toast.LENGTH_SHORT).show()
+        }
+        binding.layoutDrawer.closeDrawers()
+        return false
+    }
 
+    override fun onDestroy() {
+        mMainBinding = null
+        super.onDestroy()
+    }
+
+    override fun onBackPressed() {
+        if(binding.layoutDrawer.isDrawerOpen(GravityCompat.END))
+        {
+            binding.layoutDrawer.closeDrawers()
+        }
+        else
+        {
+            super.onBackPressed()
+        }
     }
 }
